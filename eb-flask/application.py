b35f6223938ab4f5b3eda6c1e6633d459712c910
asimshrestha2 <asims178@gmail.com>
2017-03-31 14:55:04 -0400

    Add Login page [for testing]
    +Login Page
    +Fix for static file giving error

--- eb-flask/application.py
+++ eb-flask/application.py
@@ -1,5 +1,5 @@
 from flask import Flask, session, render_template, redirect, url_for, escape, request
-
+import os
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
 
@@ -20,9 +20,33 @@ def event(name=None):
     else:
         return render_template('eventpage.html', name=name)
 
+# Function for all the login user page
+@application.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return "POST Login"
+    else:
+        return render_template('login.html')
+
 @application.errorhandler(404)
 def page_not_found(error):
     return render_template('index.html'), 404
+
+@application.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(application.root_path, endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+# TODO: Need database class that handles all the database commands and connection
+# TODO: Need a social media api handling class
+# TODO: Handle the Money API
 
 # run the app.
 if __name__ == "__main__":
