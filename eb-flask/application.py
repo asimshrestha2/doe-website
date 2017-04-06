17df04a399cc25417233e33fd4ba66aff1255d52
a2j2p4ftw <apanei224@gmail.com>
2017-04-06 18:13:50 -0500

    Login/Logout Functions
    Added base functions
    Need better secret key and login integration

--- eb-flask/application.py
+++ eb-flask/application.py
@@ -5,6 +5,8 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
     return render_template('index.html')
 
 @application.route('/u/')
@@ -39,9 +41,19 @@ def event(name=None):
 @application.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return "POST Login"
+        session['username'] = request.form['username']
+        return redirect(url_for('hello'))
     else:
         return render_template('login.html')
+
+@application.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('hello'))
+
+# set the secret key.  keep this really secret:
+application.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 @application.errorhandler(404)
 def page_not_found(error):
