5f322867208208162882546400c1a6b41ff3ba31
Cyphina <luvkittykittens@gmail.com>
2017-04-15 16:26:48 -0400

    Revert "ClosedDBConnection"
    This reverts commit 4e8917aa4851c5e8125702bc009319a59347d072.

--- eb-flask/dbconnection.py
+++ eb-flask/dbconnection.py
@@ -1,14 +1,9 @@
 import MySQLdb
 
 def executeQuery(query):
-    db = MySQLdb.connect()
-
     if query:
         c=db.cursor()
         c.execute("""{}""".format(query))
         return c.fetchall()
     else:
         raise QueryError('Query Missing')
-
-    c.close()
-    db.close()
