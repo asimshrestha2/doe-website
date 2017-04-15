4e8917aa4851c5e8125702bc009319a59347d072
Cyphina <luvkittykittens@gmail.com>
2017-04-15 16:21:07 -0400

    ClosedDBConnection
    Added gitignore and closed database connection

--- eb-flask/dbconnection.py
+++ eb-flask/dbconnection.py
@@ -1,9 +1,14 @@
 import MySQLdb
 
 def executeQuery(query):
+    db = MySQLdb.connect()
+
     if query:
         c=db.cursor()
         c.execute("""{}""".format(query))
         return c.fetchall()
     else:
         raise QueryError('Query Missing')
+
+    c.close()
+    db.close()
